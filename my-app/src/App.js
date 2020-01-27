96a886f9594dc77115e73427472aed87d48b20e5
Ashley Roarick <ahorseroar@gmail.com>
2020-01-27 16:02:00 -0600

    added title

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import Container from "react-bootstrap/Container";
+
+function App() {
+  return (
+    <>
+      <Container>
+        <div className="title-part-one">
+          <h1>In</h1>
+        </div>
+        <div className="title-part-two">
+          <h1>Concert</h1>
+        </div>
+      </Container>
+    </>
+  );
+}
+
+export default App;
