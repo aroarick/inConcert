1af9fada3d38deeadeabd33a89cf1ff569f6bc15
Ashley Roarick <ahorseroar@gmail.com>
2020-01-29 15:40:20 -0600

    animations

--- my-app/src/components/Home/home.js
+++ my-app/src/components/Home/home.js
@@ -9,21 +9,95 @@ function Home() {
     <>
       <Container fluid>
         <Row className="row-1">
-          <Col>
-            <h4 className="col-1-text" id="inConcert">
+          <Col className="row-1-col-1">
+            <h5 className="col-1-text" id="inConcert">
               In Concert
-            </h4>
-            <h4 className="col-1-text" id="findConcert">
+            </h5>
+            <h5 className="col-1-text" id="findConcert">
               Find your concert
-            </h4>
+            </h5>
+          </Col>
+          <Col className="row-1-col-2">
+            <h5 className="col-2-text" id="inConcert">
+              Concerts In:
+            </h5>
+            <h5 className="col-2-text" id="findConcert">
+              Dallas Area
+            </h5>
+          </Col>
+          <Col className="row-1-col-3"></Col>
+        </Row>
+        <Row className="artist-row">
+          <button>
+            <Col>
+              <h2 className="artist-1">Halsey</h2>
+            </Col>
+          </button>
+          <button>
+            <Col>
+              <h2 className="artist">The 1975</h2>
+            </Col>
+          </button>
+          <button>
+            <Col>
+              <h2 className="artist">Nickelback</h2>
+            </Col>
+          </button>
+          <button>
+            <Col>
+              <h2 className="artist">Steve Aokli</h2>
+            </Col>
+          </button>
+        </Row>
+        <Row className="artist-row">
+          <button>
+            <Col>
+              <h2 className="artist-1">Alicia Keys</h2>
+            </Col>
+          </button>
+          <button>
+            <Col>
+              <h2 className="artist">BTS</h2>
+            </Col>
+          </button>
+        </Row>
+        <Row className="artist-row">
+          <button>
+            <Col>
+              <h2 className="artist-1">Bon Jovi and Bryan Adams</h2>
+            </Col>
+          </button>
+          <button>
+            <Col>
+              <h2 className="artist">Of Monsters and Men</h2>
+            </Col>
+          </button>
+        </Row>
+        <Row className="festival-row">
+          <button>
+            <Col>
+              <h2 className="artist-1">Coachella</h2>
+            </Col>
+          </button>
+          <button>
+            <Col>
+              <h2 className="festival">Inkcarnation</h2>
+            </Col>
+          </button>
+          <button>
+            <Col>
+              <h2 className="festival">Innings Festival 2020</h2>
+            </Col>
+          </button>
+        </Row>
+      </Container>
+      <Container fluid>
+        <Row className="row-2">
+          <Col className="row-1-col-1">
+            <h5>Sign Up</h5>
           </Col>
           <Col>
-            <h4 className="col-2-text" id="inConcert">
-              Concerts In:
-            </h4>
-            <h4 className="col-2-text" id="findConcert">
-              Dallas Area
-            </h4>
+            <h5>About In Concert</h5>
           </Col>
         </Row>
       </Container>
